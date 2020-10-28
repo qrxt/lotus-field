a948fe12a8e7799794399183e90e0cad54c110e5
qrxt <qrx357@gmail.com>
2020-10-28 17:41:50 +0300

    исправляет размер, подсветку пунктов меню в навигации. Исправляет отсутствие закрытия навигации при выборе пункта меню

--- src/components/nav-bar/nav-bar.jsx
+++ src/components/nav-bar/nav-bar.jsx
@@ -18,9 +18,9 @@ const MyNavbar = () => {
   const { t } = useTranslation();
 
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="light" expand="lg" collapseOnSelect>
       <Navbar.Brand>
-        <Link to="/">
+        <Nav.Link to="/" as={ Link } className={ styles.logo }>
           <img
             src={ logo }
             className={ styles.logo }
@@ -28,29 +28,29 @@ const MyNavbar = () => {
             height="48"
             alt="Logo"
           />
-        </Link>
+        </Nav.Link>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <ul className="navbar-nav">
-            <li className="nav-item active">
-              <NavLink icon={ faHome } to="/" isCurrent>
+            <li className="nav-item">
+              <NavLink icon={ faHome } to="/" isCurrent eventKey="1">
                 { t('nav-menu.home') }
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink icon={ faHeart } to="/wishlist">
+              <NavLink icon={ faHeart } to="/wishlist" eventKey="2">
                 { t('nav-menu.wishlist') }
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink icon={ faSearch } to="/search">
+              <NavLink icon={ faSearch } to="/search" eventKey="3">
                 { t('nav-menu.search') }
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink icon={ faCog } to="/settings">
+              <NavLink icon={ faCog } to="/settings" eventKey="4">
                 { t('nav-menu.settings') }
               </NavLink>
             </li>
