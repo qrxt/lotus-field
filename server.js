a3775d03f3b8a84520ae6c28d463830f683a884b
qrxt <qrx357@gmail.com>
2020-11-24 18:11:14 +0300

    исправляет отступы в мобильной версии, исправляет gzip-компрессию

--- server.js
+++ server.js
@@ -8,6 +8,27 @@ const dirname = path.resolve(path.dirname(''));
 
 const app = express();
 
+app.get('*.js', (req, res, next) => {
+  req.url = `${req.url}.gz`;
+  res.set('Content-Encoding', 'gzip');
+  res.set('Content-Type', 'text/javascript');
+  next();
+});
+
+app.get('*.css', (req, res, next) => {
+  req.url = `${req.url}.gz`;
+  res.set('Content-Encoding', 'gzip');
+  res.set('Content-Type', 'text/css');
+  next();
+});
+
+app.get(/.*[eot|ttf|woff|woff2]$/, (req, res, next) => {
+  req.url = `${req.url}.gz`;
+  res.set('Content-Encoding', 'gzip');
+  res.set('Content-Type', 'text/css');
+  next();
+});
+
 app.use(favicon(`${dirname}/public/img/lotus.svg`));
 app.use(express.static(dirname));
 app.use(express.static(path.join(dirname, 'dist')));
@@ -17,8 +38,8 @@ app.get('/*', gzipStatic('./dist/', {
   orderPreference: ['gz'],
 }));
 
-app.get('/wakemydino.txt', (req, res) => {
-  res.sendFile(path.join(dirname, 'wakemydino.txt'));
+app.get('/wakemydyno.txt', (req, res) => {
+  res.sendFile(path.join(dirname, 'wakemydyno.txt'));
 });
 
 app.listen(port);
